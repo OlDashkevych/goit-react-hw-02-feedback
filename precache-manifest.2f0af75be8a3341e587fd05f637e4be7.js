d1f7a58f00c05b2eadffda581a208f50efbaaa34
OlDashkevych <dashkevich672@gmail.com>
2019-12-12 01:15:20 +0200

    Updates

---
+++ precache-manifest.2f0af75be8a3341e587fd05f637e4be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1f6a764ec8e27aaa441fcde3396ae4",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "f6b43ae2e79925352475",
+    "url": "/goit-react-hw-02-feedback/static/css/main.383b5d7c.chunk.css"
+  },
+  {
+    "revision": "61a8731a6ccb755f4ff7",
+    "url": "/goit-react-hw-02-feedback/static/js/2.745df647.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.745df647.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6b43ae2e79925352475",
+    "url": "/goit-react-hw-02-feedback/static/js/main.64f1eb70.chunk.js"
+  },
+  {
+    "revision": "aad533a6b43c150c6739",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.cc2e1481.js"
+  }
+]);
